3bcafa2a7d30b672690e32841a78a0d790a1d4de
Webordable <webordable@gmail.com>
2026-03-19 03:06:56 -0300

    Update Craftsmanship.tsx

--- src/app/pages/Craftsmanship.tsx
+++ src/app/pages/Craftsmanship.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'motion/react';
 import { useLanguage } from '../contexts/LanguageContext';
 import { Link } from 'react-router';
-const finishingImage = "/src/assets/images/e3d847519aacc2781dd5930d61ae7692026785b7.png";
+import finishingImage from "../assets/images/e3d847519aacc2781dd5930d61ae7692026785b7.png";
 
 export default function Craftsmanship() {
   const { t } = useLanguage();
